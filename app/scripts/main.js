c0d4262387af53e2727b3e9cdf62b00a84592e51
Luis Mendoza <luis@mendoza.io>
2015-01-26 12:12:04 -0600

    Added new Photos view.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,20 +19,35 @@ $(document).ready(function () {
     // Routes
     var Router = Backbone.Router.extend({
         routes: {
-            '' : 'home'
+            '' : 'home',
+            'event/:id' : 'event'
         },
 
         home: function(){
             console.log('home!');
             // Call render
             eventsView.render();
+        },
+
+        event: function(id){
+            console.log('events! ' + id);
+            photosView.render({id: id});
         }
     });
 
+    // ---- MODELS ---- //
+
     // Event Model
     var Event = Parse.Object.extend({
-        className: 'Evento',
+        className: 'Evento'
     });
+
+    // Photo Model
+    var Photo = Parse.Object.extend({
+        className: 'Photo'
+    });
+
+    // ---- COLLECTIONS ---- //
 
     // Event Collection
     var Events = Parse.Collection.extend({
@@ -40,10 +55,19 @@ $(document).ready(function () {
         query: (new Parse.Query(Event))
     });
 
+    var Photos = Parse.Collection.extend({
+        model: Photo,
+        query: (new Parse.Query(Photo))
+    });
+
+    // ---- VIEWS ---- //
+
     // Event View
     var EventsView = Parse.View.extend({
         // Container element
         el: '#event-app',
+
+        className: 'event',
 
         // Render function
         render: function(){
@@ -63,12 +87,108 @@ $(document).ready(function () {
                 }
 
             });
+        },
+
+        events: {
+            'click .event' : 'eventSelect'
+        },
+
+        eventSelect: function(){
+            console.log('selected event!');
+            var eventId = this.$('.event-id').val();
+            this.showPhotos(eventId);
+        },
+
+        showPhotos: function(eventId){
+            console.log(eventId)
         }
+
+    });
+
+    // Photo View
+    var PhotosView = Parse.View.extend({
+        // Container element
+        el: '#event-app',
+
+        className: 'event',
+
+        // Render function
+        render: function(options){
+            var events = new Events(); // new object from collection
+            var that = this; // to avoid scoping issues
+            events.fetch({
+
+                success: function(events){
+                    console.log('got events!');
+                    // Create underscore template and pass template jquery id, scope the object to only pass models
+                    //var template = _.template( $('#event-list').html(), { events : events.models } );
+                    //that.$el.html(template);
+
+                    var query = new Parse.Query(Photo);
+                    query.equalTo('eventId', options.id);
+                    var collection = query.collection();
+                    collection.fetch({
+
+                        success: function(photos){
+                            console.log('got photos!');
+                            // Create underscore template and pass template jquery id, scope the object to only pass models
+                            var template = _.template( $('#photo-event-list').html(), { photos : photos.models, events : events.models } );
+                            that.$el.html(template);
+                        },
+
+                        error: function(){
+                            alert('Unable to retrieve data.');
+                        }
+
+                    });
+                },
+
+                error: function(){
+                    alert('Unable to retrieve data.');
+                }
+
+            });
+
+            /*
+            var query = new Parse.Query(Photo);
+            query.equalTo('eventId', options.id);
+            var collection = query.collection();
+            collection.fetch({
+
+                success: function(photos){
+                    console.log('got photos!');
+                    // Create underscore template and pass template jquery id, scope the object to only pass models
+                    var template = _.template( $('#photo-list').html(), { photos : photos.models } );
+                    that.$el.html(template);
+                },
+
+                error: function(){
+                    alert('Unable to retrieve data.');
+                }
+
+            });*/
+        },
+
+        events: {
+            'click .event' : 'eventSelect'
+        },
+
+        eventSelect: function(){
+            console.log('selected event!');
+            var eventId = this.$('.event-id').val();
+            this.showPhotos(eventId);
+        },
+
+        showPhotos: function(eventId){
+            console.log(eventId)
+        }
+
     });
 
     // Instances
     var router = new Router();
     var eventsView = new EventsView();
+    var photosView = new PhotosView();
 
     // Start Backbone
     Backbone.history.start();
